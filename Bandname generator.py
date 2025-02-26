9896c6c540c46d28057181df9e0765ad47535da7
Axiel Urenda Miranda <axielurendamiranda@gmail.com>
2025-02-26 14:14:16 -0600

    Add files via upload

---
+++ Bandname generator.py
@@ -0,0 +1,6 @@
+
+print('Welcome to the Bandname generator\n')
+city = input('Where was the city where you grew up?\n')
+pet = input("what was the name of your pet?\n")
+
+print(f"The name of your band is {city}{pet}")
